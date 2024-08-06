0d0a2405e069811fc3ef49c18aa7f2778d77271e
prathikhya <prathikhyadevi25@gmail.com>
2024-08-06 12:10:06 +0530

    added and renamed files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
 import ProductTab from './ap/ProductTab';
-
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Index from './pages/home/Index';
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Index/>,
+    },
+  ]);
+
   return (
     <>
       {/* <ProductTab/>  */}
-
+      
+      <RouterProvider router={router} />
     </>
    
   );
---
+++ src/pages/home/Index.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import { createBrowserRouter } from "react-router-dom";
+function Index() {
+  return (
+    <div>
+      <h1>welcome</h1>
+    </div>
+  )
+}
+
+export default Index;
